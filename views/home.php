90a1bc7d1476d50c3bbaa5cfb8e3dc0f12416396
tomkoker <teddy.koker@gmail.com>
2016-02-08 17:07:45 -0500

    refactored home.php

--- views/home.php
+++ views/home.php
@@ -11,26 +11,29 @@
   </fieldset>
 </form>
 <ul class="list-group">
-  <?php
-  foreach ($posts as $post)
-  {
-    print("<li class='list-group-item' id='post-".$post["id"]."'>");
-    print("<span class='badge'>");
-    print("<span class='likes' id='likes-".$post["id"]."'>".$post["likes"]."</span>");
-    print("</span>");
-    print("<div><a href='user.php?id=".$post["user"]["id"]."'><strong>".$post["user"]["username"]."</strong></a> <small>". date_format(date_create($post["date"]),"n/j/y, g:ia")."</small></div>");
-    print("<div>".$post["text"]."</div>");
-    print("<div>");
-    if(!$post["liked"])
-    {
-      print("<a class='like-button' id='like-button-".$post["id"]."'href='/like.php?id=".$post["id"]."'>Like <span class='glyphicon glyphicon-thumbs-up'></span></a>");
-    }
-    else
-    {
-      print("<a class='unlike-button' id='unlike-button-".$post["id"]."'href='/like.php?id=".$post["id"]."'>Unlike</a>");
-    }
-    print("</div>");
-    print("</li>");
-  }
-  ?>
+  <?php foreach ($posts as $post){ ?>
+    <li class='list-group-item' id='post-<?=$post["id"]?>'>
+      <span class='badge'>
+        <span class='likes' id='likes-".$post["id"]."'><?=$post["likes"]?></span>
+      </span>
+      <div>
+        <a href='user.php?id=<?=$post["user"]["id"]?>'><strong><?=$post["user"]["username"]?></strong></a> <small><?=date_format(date_create($post["date"]),"n/j/y, g:ia")?></small>
+      </div>
+      <div>
+        <?=$post["text"]?>
+      </div>
+      <div>
+      <?php
+        if(!$post["liked"])
+        {
+          print("<a class='like-button' id='like-button-".$post["id"]."'href='/like.php?id=".$post["id"]."'>Like <span class='glyphicon glyphicon-thumbs-up'></span></a>");
+        }
+        else
+        {
+          print("<a class='unlike-button' id='unlike-button-".$post["id"]."'href='/like.php?id=".$post["id"]."'>Unlike</a>");
+        }
+      ?>
+      </div>
+    </li>
+  <?php }?>
 </ul>
