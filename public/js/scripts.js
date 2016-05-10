ef121ecb6453c1779bf6e532a9b93b390917aed1
tomkoker <teddy.koker@gmail.com>
2016-05-10 19:42:56 -0400

    better topic page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -28,25 +28,48 @@ function like(postID)
 }
 function follow(topicID)
 {
-
+  $.ajax({
+    url: "follow.php",
+    data: "id=" + topicID,
+    type: "POST",
+    success:function(){
+      $('#follow-' + topicID).replaceWith("<a id='unfollow-" + topicID + "' class='unfollow btn btn-default' href='#'>Unfollow</a>");
+    }
+  });
 }
 function unfollow(topicID)
 {
-
+  $.ajax({
+    url: "follow.php",
+    data: "id=" + topicID,
+    type: "POST",
+    success:function(){
+      $('#unfollow-' + topicID).replaceWith("<a id='follow-" + topicID + "' class='follow btn btn-primary' href='#'>Follow</a>");
+    }
+  });
 }
 
 
 $(document).ready(function(){
   $('.post').on('click', '.like-button', function(){
-    console.log(this);
     postID = $(this).attr('id').replace('like-button-', '');
     like(postID);
     return false;
   });
   $('.post').on('click', '.unlike-button', function(){
     postID = $(this).attr('id').replace('unlike-button-', '');
-    console.log(this);
     unlike(postID);
     return false;
   });
+  $('.topic-header').on('click', '.follow', function(){
+    topicID = $(this).attr('id').replace('follow-', '');
+    follow(topicID);
+    return false;
+  });
+  $('.topic-header').on('click', '.unfollow', function(){
+    topicID = $(this).attr('id').replace('unfollow-', '');
+    unfollow(topicID);
+    return false;
+  });
+
 });
--- views/topic.php
+++ views/topic.php
@@ -1,7 +1,14 @@
-<h1><?=$topic["name"]?></h1>
-<p>
-  <a class="btn btn-default" href="#">Follow</a>
-</p>
+<div class="jumbotron topic-header">
+  <h1><?=$topic["name"]?></h1>
+  <p><?=$topic["description"]?></p>
+  <p>
+    <?php if($followed): ?>
+      <a id="unfollow-<?=$topic["id"]?>" class="unfollow btn btn-default" href="#">Unfollow</a>
+    <?php else: ?>
+      <a id="follow-<?=$topic["id"]?>" class="follow btn btn-primary" href="#">Follow</a>
+    <?php endif; ?>
+  </p>
+</div>
 <form action="post.php" method="post">
   <fieldset>
     <input type="hidden" name="topic" value="<?=$topic["id"]?>" />
