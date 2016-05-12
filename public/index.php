86db37bcdc4eefc567c67d382261faa4c6820c17
tomkoker <teddy.koker@gmail.com>
2016-05-12 01:34:04 -0400

    page=number starting to work

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
   // configuration
   require("../includes/config.php");
 
-  $rows = Lib::query("SELECT * FROM posts");
+  $page = isset($_GET["page"]) && (intval($_GET["page"]) > 1) ? $_GET["page"] : 1;
+  $start = ($page-1) * 10;
+
+  $rows = Lib::query("SELECT * FROM posts ORDER BY id DESC LIMIT " . $start . ", 10");
   $posts = formPosts($rows);
   render("home.php", ["title" => "Home", "posts" => $posts]);
 
--- views/posts_list.php
+++ views/posts_list.php
@@ -21,9 +21,3 @@
     </li>
   <?php endforeach; ?>
 </ul>
-<nav>
-  <ul class="pager">
-    <li class="previous"><a href="#">Previous</a></li>
-    <li class="next"><a href="#">Next</a></li>
-  </ul>
-</nav>
