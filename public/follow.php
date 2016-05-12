73dd59305c3aebad0bc14673a43c71b5f9930bf2
tomkoker <teddy.koker@gmail.com>
2016-05-12 10:01:18 -0400

    fixed follow bug

--- public/follow.php
+++ public/follow.php
@@ -3,20 +3,19 @@
 // configuration
 require("../includes/config.php");
 
-if(isset($_GET["id"]))
+if(isset($_POST["id"]))
 {
-  $rows = Lib::query("SELECT * FROM followers WHERE user_id = ? AND topic_id = ?", $_SESSION["id"], $_GET["id"]);
+  $rows = Lib::query("SELECT * FROM followers WHERE user_id = ? AND topic_id = ?", $_SESSION["id"], $_POST["id"]);
 
   //if already followed, unfollow
   if( count($rows) > 0)
   {
-    $result = Lib::query("DELETE FROM followers WHERE user_id = ? AND topic_id = ?", $_SESSION["id"], $_GET["id"]);
+    $result = Lib::query("DELETE FROM followers WHERE user_id = ? AND topic_id = ?", $_SESSION["id"], $_POST["id"]);
   }
-  //else like it
+  //else follow it
   else
   {
-   $result = Lib::query("INSERT IGNORE INTO followers (user_id, topic_id) VALUES (?, ?)", $_SESSION["id"], $_GET["id"]);
+   $result = Lib::query("INSERT IGNORE INTO followers (user_id, topic_id) VALUES (?, ?)", $_SESSION["id"], $_POST["id"]);
   }
 }
-redirect("/");
 ?>
