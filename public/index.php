287ca5e692371387e62e7c6f7d9e3164a812ff13
tomkoker <teddy.koker@gmail.com>
2016-05-13 19:52:47 -0400

    only show followed topics on homepage

--- public/index.php
+++ public/index.php
@@ -5,12 +5,16 @@
 
   $limit = 25;
 
-  $counts = Lib::query("SELECT COUNT(*) AS count FROM posts");
+  $followed = Lib::query("SELECT * FROM followers WHERE user_id = ?", $_SESSION["id"]);
+  $followed = array_column($followed, "topic_id");
+  $followed = implode(", " , $followed);
+
+  $counts = Lib::query("SELECT COUNT(*) AS count FROM posts  WHERE topic_id IN (" . $followed . ")");
   $count = (int) $counts[0]["count"];
 
   $info = pageInfo($limit, $count);
 
-  $rows = Lib::query("SELECT * FROM posts ORDER BY id DESC LIMIT " . $info["start"] . ", " . $limit);
+  $rows = Lib::query("SELECT * FROM posts WHERE topic_id IN (" . $followed . ") ORDER BY id DESC LIMIT " . $info["start"] . ", " . $limit);
   $posts = formPosts($rows);
 
   render("home.php", ["title" => "Home", "posts" => $posts, "page" => $info["page"], "last" => $info["last"]]);
