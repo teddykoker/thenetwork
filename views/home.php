8e6ed39aec3e85d5fcd0328804939e647979a551
tomkoker <teddy.koker@gmail.com>
2016-05-16 11:23:59 -0400

    message to new users

--- views/home.php
+++ views/home.php
@@ -1,1 +1,5 @@
-<?php require('posts_list.php') ?>
+<?php if(count($posts) == 0): ?>
+  <div class="alert alert-info">Start following some <a class="alert-link" href="/topics.php">topics</a> to see posts!</div>
+<?php else: ?>
+  <?php require('posts_list.php') ?>
+<?php endif; ?>
